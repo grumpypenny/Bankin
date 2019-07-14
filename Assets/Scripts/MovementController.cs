e2fa648dc8a48546e56d4a464c309e183eb1c229
grumpypenny <ajitesh.misra.14@gmail.com>
2019-07-14 14:06:36 -0400

    Initial commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This class works by firing rays from the circle collider's
+ * surface in the direction of travel
+ * 
+ * Note: Call move from FIXED UPDATE so that everything works
+ */ 
+
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class MovementController : MonoBehaviour
+{
+	[Header("Mask")]
+	public LayerMask CollisionMask;
+	public LayerMask selfMask;
+
+	const float skinWidth = 0.1f;
+
+	[Space]
+
+	[Header("Resolution")]
+	public int numberOfRays;
+
+	private float raySpacing;
+	private CircleCollider2D circle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		circle = GetComponent<CircleCollider2D>();
+		selfMask = LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer));
+		FindRaySpacing();
+    }
+
+	// assumes script calling this function already
+	// multiplied by Time.deltatime
+	public void Move(Vector3 velocity)
+	{
+		FireRays(ref velocity);
+
+		transform.Translate(velocity);
+	}
+
+	private void FireRays(ref Vector3 velocity)
+	{
+		Debug.DrawRay(transform.position, velocity.normalized * circle.radius, Color.red);
+
+		float rayLength = velocity.magnitude + skinWidth;
+
+		Ray2D travelDirection = new Ray2D(gameObject.transform.position, velocity.normalized);
+		Vector3 pointOnCircle = travelDirection.GetPoint(circle.radius - skinWidth);
+
+		Debug.DrawLine(gameObject.transform.position, pointOnCircle, Color.blue);
+		Debug.DrawRay(pointOnCircle, Vector2.Perpendicular(velocity.normalized) * circle.radius, Color.red);
+		Debug.DrawRay(pointOnCircle, -Vector2.Perpendicular(velocity.normalized) * circle.radius, Color.magenta);
+
+		Ray2D posPerpRay = new Ray2D(pointOnCircle, Vector2.Perpendicular(velocity.normalized));
+		for (int j = -1; j <= 1; j+= 2)
+		{
+			for (int i = j; i < numberOfRays; i++)
+			{
+				if (i < 0)
+				{
+					continue;
+				}
+
+				Vector2 rayOrigin = pointOnCircle;
+				rayOrigin += j * posPerpRay.direction * (raySpacing * i + Vector2.Dot(posPerpRay.direction, velocity));
+
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, velocity.normalized, rayLength, CollisionMask);
+				Debug.DrawRay(rayOrigin, velocity.normalized * rayLength, Color.cyan);
+
+				if (hit)
+				{
+					rayLength = hit.distance;
+					velocity = velocity * (rayLength - skinWidth);
+					print(hit.transform.name);
+				}
+			}
+		}
+	}
+
+	// Calculate how far apart the rays should be
+	private void FindRaySpacing()
+	{
+		numberOfRays = Mathf.Clamp(numberOfRays, 2, int.MaxValue);
+
+		raySpacing = circle.radius / (numberOfRays - 1);
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementController))]
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+
+	private MovementController movement;
+	private Vector2 input;
+
+	private void Start()
+	{
+		movement = GetComponent<MovementController>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+
+	private void FixedUpdate()
+	{
+		movement.Move(input.normalized * speed * Time.deltaTime);
+	}
+
+}
